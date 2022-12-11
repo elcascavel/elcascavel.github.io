38493ecfc5c1227f2f9953782447ceee69335b56
Cascavel <simao.ramaral@gmail.com>
2022-12-11 19:10:33 +0000

    Experience

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navigation from "./components/_organisms/Navbar/Navbar";
 import Header from "./components/_organisms/Header/Header";
 import About from "./components/_organisms/About/About";
 import Projects from "./components/_organisms/Projects/Projects";
-import Experience from "./components/Experience";
+import Experience from "./components/_organisms/Experience/Experience";
 
 import "./fontawesome";
 
@@ -22,6 +22,7 @@ const App = () => {
       <Header header={header} />
       <About about={about} />
       <Projects projects={projects} />
+      <Experience experience={experience} />
     </div>
   );
 };
---
+++ src/components/_organisms/Experience/Experience.jsx
@@ -0,0 +1,43 @@
+import { Tab } from "@headlessui/react";
+import styles from "./Experience.module.scss";
+
+const Experience = (props) => {
+  return (
+    <div className={styles["container"]}>
+      <h2 className={styles["title"]}>3. Relevant Experience</h2>
+      <div className={styles["tab-group"]}>
+        <Tab.Group>
+          <Tab.List>
+            {props.experience.map((experience) => {
+              return (
+                <Tab className={styles["tab-button"]} key={experience.company}>
+                  {experience.company}
+                </Tab>
+              );
+            })}
+          </Tab.List>
+          <Tab.Panels>
+            {props.experience.map((experience) => {
+              return (
+                <Tab.Panel key={experience.id}>
+                  <p>
+                    {experience.title}{" "}
+                    <span className={styles["workplace"]}>
+                      @ {experience.company}
+                    </span>
+                  </p>
+                  <ul>
+                    {experience.tasks.map((task) => {
+                      return <li key={task}>{task}</li>;
+                    })}
+                  </ul>
+                </Tab.Panel>
+              );
+            })}
+          </Tab.Panels>
+        </Tab.Group>
+      </div>
+    </div>
+  );
+};
+export default Experience;
