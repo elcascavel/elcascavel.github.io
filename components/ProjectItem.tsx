bee3cae169e2f54829a0a91efe67bdd6accc9c76
Cascavel <simao.ramaral@gmail.com>
2023-06-11 18:26:07 +0100

    Style change project item

--- components/ProjectItem.tsx
+++ components/ProjectItem.tsx
@@ -40,7 +40,7 @@ const ProjectItem: React.FC<ProjectItemProps> = ({ data, onClick }) => {
           alt="Image"
         />
       </div>
-      <div className="flex flex-col items-start w-full p-4 gap-y-1">
+      <div className="flex flex-col items-start w-full py-4 gap-y-1">
         <p className="font-semibold truncate w-full">{data.title}</p>
         <p className="text-neutral-400 text-sm w-full line-clamp-3">
           {data.description}
