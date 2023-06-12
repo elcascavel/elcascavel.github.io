59d77cdf2b2a6292865a8fce942ed81cd93f1f57
Cascavel <simao.ramaral@gmail.com>
2023-06-13 00:35:28 +0100

    Greeting based on time of day

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -2,6 +2,7 @@ import getProjects from "@/actions/getProjects";
 import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
 import PageContent from "./components/PageContent";
+import Greeting from "@/components/Greeting";
 
 export const revalidate = 0;
 
@@ -20,7 +21,9 @@ export default async function Home() {
     >
       <Header>
         <div className="mb-2">
-          <h1 className="text-white text-3xl font-semibold">Welcome back</h1>
+          <h1 className="text-white text-3xl font-semibold">
+            <Greeting />
+          </h1>
           <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
             <ListItem image="/images/liked.png" name="Next.js" href="liked" />
           </div>
---
+++ components/Greeting.tsx
@@ -0,0 +1,30 @@
+interface GreetingProps {}
+
+const Greeting: React.FC<GreetingProps> = () => {
+  const currentHour = new Date().getHours();
+
+  console.log(currentHour);
+  let timeOfDay;
+
+  if (currentHour >= 5 && currentHour < 12) {
+    timeOfDay = "morning";
+  } else if (currentHour >= 12 && currentHour < 18) {
+    timeOfDay = "afternoon";
+  } else {
+    timeOfDay = "evening";
+  }
+
+  let greeting;
+
+  if (timeOfDay === "morning") {
+    greeting = "Good morning!";
+  } else if (timeOfDay === "afternoon") {
+    greeting = "Good afternoon!";
+  } else {
+    greeting = "Good evening!";
+  }
+
+  return greeting;
+};
+
+export default Greeting;
